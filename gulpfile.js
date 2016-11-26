91c17d399bf887a8c6738ebd91da247a07ff5980
ATakaSKY <thakurakash321@gmail.com>
2016-11-27 00:02:53 +0530

    Added Live reload

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
        coffee = require('gulp-coffee'),
        concat = require('gulp-concat'),
        browserify = require('gulp-browserify'),
-       compass = require('gulp-compass');
+       compass = require('gulp-compass'),
+       connect = require('gulp-connect');
 
        var coffeeSources = ['components/coffee/tagline.coffee'];
 
@@ -12,23 +13,27 @@ var gulp = require('gulp'),
        	'components/scripts/pixgrid.js',
        	'components/scripts/tagline.js',
        	'components/scripts/template.js'
-       ]
+       ];
 
-       var sassSources = ['components/sass/style.scss']
+       var sassSources = ['components/sass/style.scss'];
+
+       var htmlSources = ['builds/development/*.html'];
+       var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee',function(){
 	gulp.src(coffeeSources)
 	.pipe(coffee({bare:true})
 	  .on('error',gutil.log))
 	.pipe(gulp.dest('components/scripts'))
-})
+});
 
 gulp.task('js',function(){
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
 	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
-})
+	.pipe(connect.reload());
+});
 
 gulp.task('compass', function(){
 	gulp.src(sassSources)
@@ -39,13 +44,33 @@ gulp.task('compass', function(){
 	})
 	  .on('error',gutil.log))
 	.pipe(gulp.dest('builds/development/css'))
-})
+	.pipe(connect.reload());
+});
 
 gulp.task('watch',function(){
 	gulp.watch(coffeeSources,['coffee']);
 	gulp.watch(jsSources,['js']);
 	gulp.watch('components/sass/*.scss',['compass']);
+	gulp.watch(htmlSources,['html']);
+	gulp.watch(jsonSources,['json']);
 });
 
+gulp.task('connect', function() {
+  connect.server({
+    root: 'builds/development/',
+    livereload: true
+  });
+});
 
-gulp.task('default',['coffee','js','compass','watch'])
+gulp.task('html', function(){
+	gulp.src(htmlSources)
+	.pipe(connect.reload());
+});
+
+gulp.task('json', function(){
+	gulp.src(jsonSources)
+	.pipe(connect.reload());
+})
+
+
+gulp.task('default',['html','json','coffee','js','compass','connect','watch'])
